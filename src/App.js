1e6845191c2f47ea5bab278575038eae9dcd2279
giulio <giulio09@marcov.it>
2020-08-02 11:58:51 +0000

    Fix algoritmo

--- src/App.js
+++ src/App.js
@@ -27,11 +27,12 @@ const inizio = new Date(2020, 6, 22);
 const oggi = new Date();
 
 function App() {
-  const quantiGiorni = (Math.floor(Math.round(Math.abs((inizio - oggi) / oneDay)) / 3) % appartamenti.length - 1);
+  console.log(oggi)
+  const quantiGiorni = (Math.floor(Math.floor(Math.abs((inizio - oggi) / oneDay)) / 4) % appartamenti.length);
   let posizione_in_data = new Array(appartamenti.length);
   for (var i = 0; i < posizione_in_data.length; i++) {
-    let pos_riga = (quantiGiorni - 1) % 3;
-    posizione_in_data[(i + quantiGiorni) % appartamenti.length] = [appartamenti[i][pos_riga], appartamenti[i][(pos_riga + 1) % 3], appartamenti[i][(pos_riga + 2) % 3]];
+    let pos_riga = quantiGiorni * 2;
+    posizione_in_data[(i + quantiGiorni) % appartamenti.length] = [appartamenti[i][pos_riga%3], appartamenti[i][(pos_riga + 1)%3], appartamenti[i][(pos_riga + 2)%3]];
   }
   return (
     <div className="App">
@@ -41,7 +42,7 @@ function App() {
         </Toolbar>
       </AppBar>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 250">
-        <path fill="#000b76" fill-opacity="1" d="M0,224L30,197.3C60,171,120,117,180,96C240,75,300,85,360,117.3C420,149,480,203,540,224C600,245,660,235,720,208C780,181,840,139,900,144C960,149,1020,203,1080,208C1140,213,1200,171,1260,160C1320,149,1380,171,1410,181.3L1440,192L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path>
+        <path fill="#000b76" fillOpacity="1" d="M0,224L30,197.3C60,171,120,117,180,96C240,75,300,85,360,117.3C420,149,480,203,540,224C600,245,660,235,720,208C780,181,840,139,900,144C960,149,1020,203,1080,208C1140,213,1200,171,1260,160C1320,149,1380,171,1410,181.3L1440,192L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path>
       </svg>
       <header className="App-header">
           <Grid
@@ -58,11 +59,10 @@ function App() {
                   direction="row"
                   justify="center"
                   alignItems="center"
-                  xs={12}
                 >
-                  <Grid item><p style={{color:"#fffff", margin:"1vw"}}>{index + 1}</p></Grid>
+                  <Grid item key={index}><p style={{color:"#fffff", margin:"1vw"}} key={index}>{index + 1}</p></Grid>
                     {
-                      righe.map(appa => <Grid item><div class="rectangle"><span>{appa}</span></div></Grid>)
+                      righe.map(appa => <Grid item key={appa}><div className="rectangle" key={appa}><span>{appa}</span></div></Grid>)
                     }
                   </Grid>
                 )
@@ -75,20 +75,18 @@ function App() {
         direction="row"
         justify="center"
         alignItems="center"
-        xs={15}
         spacing={3}
       >
-        <Grid item><div class="albero"/></Grid>
-        <Grid item><div class="albero"/></Grid>
-        <Grid item><div class="albero"/></Grid>
-        <Grid item><div class="albero"/></Grid>
+        <Grid item><div className="albero"/></Grid>
+        <Grid item><div className="albero"/></Grid>
+        <Grid item><div className="albero"/></Grid>
+        <Grid item><div className="albero"/></Grid>
       </Grid>
       <Grid 
         container
         direction="row"
-        justify="left"
+        justify="center"
         alignItems="center"
-        xs={15}
         spacing={1}
       >
         <Grid item xs><p>_ _ _ _ _ _ _ _</p></Grid>
